71fa0e95d24793c43b10c4a318ab65efc0366416
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-03-20 16:25:09 -0300

    Consolidador

--- consolidador_processamento_de_vendas.sh
+++ consolidador_processamento_de_vendas.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-cat vendas/relatorio.txt >> final.txt
+# Criando ou limpando o arquivo de relatório final
+> vendas/backup/relatorio-final.txt
+
+# Consolidando relatórios em um único arquivo
+for relatorio in vendas/backup/relatorios-*.txt; do
+    echo "Relatório: $relatorio" >> vendas/backup/relatorio-final.txt
+    echo "" >> vendas/backup/relatorio-final.txt
+    cat "$relatorio" >> vendas/backup/relatorio-final.txt
+    echo "" >> vendas/backup/relatorio-final.txt
+    echo "-----------------------------------------------" >> vendas/backup/relatorio-final.txt
+    echo "" >> vendas/backup/relatorio-final.txt
+done
